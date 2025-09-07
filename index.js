382c36590c153464f40c762b86eb71f56b2b7489
shorifulbd1st <shorifulbd1st@gmail.com>
2025-09-07 11:28:09 +0600

    GET /reviews to fetch all reviews from the database

--- index.js
+++ index.js
@@ -133,6 +133,11 @@ async function run() {
             const result = await reviewCollection.insertOne(review);
             res.send(result);
         })
+
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");

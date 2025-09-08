93d8c76cf70113f641d8ad4e9b84e634e98be822
shorifulbd1st <shorifulbd1st@gmail.com>
2025-09-08 19:45:37 +0600

    complete college registration update & delete

--- index.js
+++ index.js
@@ -163,10 +163,22 @@ async function run() {
                 }
             };
             const result = await applyColleges.updateOne(query, updateDoc);
-            console.log(result)
+
             res.send(result)
 
         })
+
+        app.delete('/delete-apply-college/:id', async (req, res) => {
+
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: new ObjectId(id) };
+            const result = await applyColleges.deleteOne(query);
+            res.send(result);
+        })
+
+
+
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
